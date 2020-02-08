23479c62fdcbfd641329143017626240e0282b0f
HrithikMittal <adhikanshmittal@gmail.com>
2020-02-08 13:12:52 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
 class App extends Component {
+  state = {
+    users: []
+  };
+
+  componentWillMount() {
+    axios
+      .get("https://api.randomuser.me/?nat=US&results=5")
+      .then(response => {
+        console.log(response);
+        this.setState({ users: response.data.results });
+      })
+      .catch(err => {
+        console.log("Error is ", err.message);
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>Hello World</h1>
       </div>
     );
   }
